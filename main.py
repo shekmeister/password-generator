d22dbb0ba7f73e7f75079367f52a91c6091831d3
shekmeister <95183266+shekmeister@users.noreply.github.com>
2022-02-05 18:36:08 +0530

    Created PassGen class.
    Created PassGen class with a function that checks if password file already exists or not.

--- main.py
+++ main.py
@@ -2,3 +2,15 @@ import os, random
 import pyAesCrypt as crypt
 import simplejson as json
 
+
+class PassGen:
+    def __init__(self, cwd) -> None:
+        self.cwd = cwd
+
+    def check_file(self):
+        pass_file = f"{self.cwd}/passwords.json"
+        if not os.path.exists(f"{self.cwd}/passwords.json"):
+            with open(pass_file, "w+") as file:
+                data = {}
+                json.dumps(file.write(data))
+    
