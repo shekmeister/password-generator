a72be6f63e342902772220e77c947a75fabdff02
shekmeister <95183266+shekmeister@users.noreply.github.com>
2022-02-05 18:39:47 +0530

    Created random password generator

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-import os, random
+import os, random, string
 import pyAesCrypt as crypt
 import simplejson as json
 
@@ -13,4 +13,11 @@ class PassGen:
             with open(pass_file, "w+") as file:
                 data = {}
                 json.dumps(file.write(data))
+
+    def create_password(self):
+        spl_chars = "!#$%&()*+,-./:;?@[]^_`~"
+        kwd = str(input("Enter a keyword for the password > ")).lower()+"@"
+        password = "".join(random.choice(string.ascii_letters + string.digits + spl_chars, k=32))
+        return kwd+password
+    
     
