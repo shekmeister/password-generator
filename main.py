2fc36bfb5476c6f416625fe75ce9ad4cf00f9c6f
shekmeister <95183266+shekmeister@users.noreply.github.com>
2022-02-06 12:01:42 +0530

    Added password saving function "write_to_self".
    Added password saving function "write_to_self" and a basic menu driven main function.

--- main.py
+++ main.py
@@ -7,6 +7,7 @@ class PassGen:
     def __init__(self, cwd) -> None:
         self.cwd = cwd
         self.pass_file = f"{self.cwd}/passwords.json"
+        self.check_file()
 
     def check_file(self):
         if not os.path.exists(self.pass_file):
@@ -17,17 +18,35 @@ class PassGen:
     def create_password(self):
         spl_chars = "!#$%&()*+,-./:;?@[]^_`~"
         kwd = str(input("Enter a keyword for the password > ")).lower()+"@"
-        password = "".join(random.choice(string.ascii_letters + string.digits + spl_chars, k=32))
+        password = "".join(random.choices(string.ascii_letters + string.digits + spl_chars, k=32))
         return kwd+password
 
+    def write_to_file(self, app, password):
+        with open(self.pass_file, "r+") as file:
+            data = json.load(file)
+            data[app] = password
+            file.seek(0)
+            json.dump(data, file)
+        
     
-    
-
 def main():
     passgen = PassGen(
-        cwd = os.getcwd()
+        cwd=os.getcwd()
     )
-    passgen.check_file()
+    app = str(input("Enter the app password is going to be used for > "))
+    while True:
+        password = passgen.create_password()
+        print(f"Your password: {password}\nAre you satisfied with this password?")
+        ans = str(input("y/n? > ")).lower()
+        if ans == "y":
+            passgen.write_to_file(app, password)
+            print("Password has been saved.")
+            break
+        elif ans == "n":
+            continue
+        else:
+            print("Invalid input")
+            continue
     
 if __name__ == "__main__":
     main()
